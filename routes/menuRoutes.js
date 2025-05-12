14583e3d7c9a3640d817975f11f067729bec80e5
ibrahimasorydiallo1 <isddiallo2003@gmail.com>
2025-05-12 18:01:51 +0200

    modify controllers

--- routes/menuRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllMenuItems } = require('../controllers/menuController');
-
-router.get('/', getAllMenuItems);
-
-module.exports = router;
